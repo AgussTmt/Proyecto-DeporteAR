401a7571903b886a07074653e35ffb3f29c1358e
AgusTMT <agusram7890@gmail.com>
2025-10-20 18:21:35 -0300

    smtp email a medio hacer

---
+++ Services (ArqBase)/Bll/GmailService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services__ArqBase_.Facade;
+
+namespace Services__ArqBase_.Bll
+{
+    internal class GmailService : MailService
+    {
+        public GmailService()
+        {
+            senderMail = ConfigurationManager.AppSettings["SenderEmail"].ToString();
+            password = ConfigurationManager.AppSettings["ContraseñaEmail"].ToString();
+            host = "smtp.gmail.com";
+            port = 587;
+            ssl = true;
+            initializeSmtpClient();
+        }
+
+
+        public string recoverPassword(string userRequesting)
+        {
+
+        }
+    }
+}
